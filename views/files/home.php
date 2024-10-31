bf5f8f169d0c5f071340e8bb0ceb4f1fa85d7a30
pgm-tesssloc <tess.slock@student.arteveldehs.be>
2024-10-31 18:09:48 +0100

    added styling to title and nav

--- views/files/home.php
+++ views/files/home.php
@@ -1,10 +1,15 @@
 <h2>Filemanager</h2>
 
-<?php foreach($list as $item): 
+<ul class="card_list">
+    <?php foreach($list as $item): 
     if ($item != '.' && $item != '..') : ?>
-        <li>   
-            <?= $item ?>
-            <a href="/files/delete/<?= $item ?>">Delete</a>
+        <li class="card file_card">  
+            <img class="file_card__img" src="/images/<?= $item ?>" alt="">
+            <div class="buttons">
+                <a class="edit_button button" href="/files/edit/<?= $item ?>">Edit</a>
+                <a class="delete_button button" href="/files/delete/<?= $item ?>">Delete</a>
+            </div>
         </li>
     <?php endif; ?>
 <?php endforeach; ?>
+</ul>
