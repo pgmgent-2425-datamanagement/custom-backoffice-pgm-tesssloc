2882ec5d2da2be2c42490bc96de07251e35e3e9f
pgm-tesssloc <tess.slock@student.arteveldehs.be>
2024-11-01 11:40:23 +0100

    added link styling

--- views/partials/stories.php
+++ views/partials/stories.php
@@ -1,6 +1,6 @@
 <div class="card">
     <h3 class="card__title card__item"><?= $story->title ?></h3>
-    <div class="card__author card__item"><?= $story->user->username ?></div>
+    <div class="card__author card__item"><a href="/users/<?= $story->user_id ?>"><?= $story->user->username ?></a></div>
     <a href="/stories/<?= $story->id ?>" class="card__link card__item">View more</a>
     <div class="buttons">
         <a class="edit_button button" href="/stories/edit/<?= $story->id ?>">Edit</a>
