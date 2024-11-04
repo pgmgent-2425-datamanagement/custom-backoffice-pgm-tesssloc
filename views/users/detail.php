37b92d5737e8c53e7c9254faaec83e317e5840e9
pgm-tesssloc <tess.slock@student.arteveldehs.be>
2024-11-04 10:31:09 +0100

    added css for user detail

--- views/users/detail.php
+++ views/users/detail.php
@@ -1,6 +1,6 @@
 <div class="detail">
     <h3 class="detail__title detail__item"><?= $user->firstName . ' ' . $user->lastName?></h3>
-    <div class="card__profilePic card__item"><img src="/images/<?= $user->profilePic ?>" alt="<?= $user->username ?>"></div>
+    <div class="card__profilePic__detail card__item"><img src="/images/<?= $user->profilePic ?>" alt="<?= $user->username ?>"></div>
     <div class="detail__content detail__item">Email: <?= $user->email ?></div>
     <div class="detail__content detail__item">Username: <?= $user->username ?></div>
     <a class="edit_button button" href="/users/edit/<?= $user->id ?>">Edit</a>
